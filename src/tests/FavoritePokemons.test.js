5dde73aab4937d142d95726a8717e2110820c5cc
LucasTapa <lucas.dfa@live.com>
2022-02-18 17:14:02 -0300

    :test_tube: Requisito 3 100%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Requisito 3 - testando o FavoritePokemons.js', () => {
+  it(`Teste se é exibido na tela a mensagem 'No favorite pokemon found',
+  se a pessoa não tiver pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pEl = screen.getByText('No favorite pokemon found');
+    expect(pEl).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const moreDetailsEl = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetailsEl);
+
+    const checkBoxEl = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(checkBoxEl);
+
+    const favoriteEl = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteEl);
+
+    const imgEl = screen.getAllByRole('img');
+    console.log(imgEl);
+    expect(imgEl).toHaveLength(2);
+  });
+});
