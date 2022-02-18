3e55b926ca7b175d91d252a68c0408acfbd21996
LucasTapa <lucas.dfa@live.com>
2022-02-18 14:56:08 -0300

    :tada: iniciando o projeto bora #VQV

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Requisito 1 - testando o App.js', () => {
+
+});
